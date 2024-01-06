96fbf8eb7c5f14a48cfe9a147167dbf4b9b254ba
indlion-program <lagolariot@gmail.com>
2024-01-06 23:31:06 +0200

    scrolling functionality, css sticking the top nav to the top opacity 30 animation for nav fixed element inside of element

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 function login_pop() {
     alert("clicked!");
 
-}
+} 
+// define elements
+var UpperBox = document.querySelector('div#upperbox');
+
+
+// all the content is loaded !
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY >= 10){
+            UpperBox.setAttribute('scrolled',true);
+            console.log("scrolled!")
+        }
+        else {
+            UpperBox.removeAttribute('scrolled');
+            console.log("removed scrolled!");
+        }
+        
+    });
+    
+  
+});
+
+
